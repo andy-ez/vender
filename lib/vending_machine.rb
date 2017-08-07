e76c90f9106ce5d236acd593b417556d82e0a89e
andy-ez <andy.eze16@gmail.com>
2017-08-07 08:40:27 +0100

    main class refactor to use delegated methods

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -12,41 +12,35 @@ require 'vending_machine_parts/display'
 class VendingMachine
   extend Forwardable
   attr_accessor :cash_register, :product_container, :calculator, :display
-  private :cash_register, :product_container, :calculator
+
   def initialize(options = {})
     coins = options[:coins] || {}
     @cash_register = VendingMachineParts::CashRegister.new(coins)
     load_change unless options[:coins]
-    @product_container = VendingMachineParts::ProductContainer.new(options[:max_size], options[:products])
+    @product_container = VendingMachineParts::ProductContainer.new(
+      options[:max_size],
+      options[:products]
+    )
     @calculator = VendingMachineParts::Calculator.new(@cash_register, @product_container)
     @display = VendingMachineParts::Display.new(@calculator)
   end
 
-  # returns the monetary value of available change in the till
-  def total_change
-    cash_register.total_value
-  end
-
-  def selection
-    calculator.selection
-  end
-
   def make_selection(product_name)
-    result = calculator.set_product(product_name)
+    result = set_product(product_name)
     return display.write_message(result) if result.is_a? String
     display.selected_product(result)
   end
 
-  def make_payment(coin, quantity=1)
-    cash_register.add_payment(coin, quantity)
-    cash_register.return_money_paid unless calculator.process_transaction
+  def make_payment(coin, quantity = 1)
+    add_payment(coin, quantity)
+    return_money_paid unless calculator.process_transaction
     return finish_transaction if transaction_successful?
     continue_transaction
   end
 
   def cancel
-    calculator.reset
-    cash_register.return_money_paid
+    reset
+    return_money_paid
   end
 
   def read_display
@@ -56,18 +50,18 @@ class VendingMachine
   def_delegators :@product_container, :add_product, :remove_product, :restock,
                  :total_quantity, :available_products, :max_size, :empty!
   def_delegators :@cash_register, :add_payment, :return_money_paid,
-                 :release_money_paid, :remove_coins
+                 :release_money_paid, :remove_coins, :pay_out_change
+  def_delegators :@calculator, :reset, :status, :change, :set_product, :selection
   # fills the cash register with coins
   def_delegator :@cash_register, :fill_register, :load_change
   def_delegator :@cash_register, :clear!, :empty_register
+  def_delegator :@cash_register, :total_value, :total_change
   # returns the coin collection representing change
   def_delegator :@cash_register, :coins, :available_change
+  private :cash_register, :product_container, :calculator, :status, :reset,
+          :pay_out_change, :change, :set_product
 
   private
-
-  def status
-    calculator.status
-  end
 
   def transaction_successful?
     status == :success
@@ -76,7 +70,7 @@ class VendingMachine
   def continue_transaction
     case status
     when :no_change
-      calculator.reset
+      reset
       display.no_change
     when :in_progress
       display.in_progress
@@ -87,16 +81,16 @@ class VendingMachine
 
   def finish_transaction
     returned_product = selection.clone
-    returned_change = calculator.change.clone
+    returned_change = change.clone
     execute_closing_steps
     [returned_product, returned_change]
   end
 
   def execute_closing_steps
     display.success
-    cash_register.release_money_paid
-    cash_register.pay_out_change(calculator.change)
+    release_money_paid # release paid in money to register
+    pay_out_change(change) # return change to customer
     remove_product(selection.name, 1)
-    calculator.reset
+    reset
   end
 end
